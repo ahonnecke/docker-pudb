a4726250660c6e88c97e195d995c4c991d683454
Ashton Honnecke <ahonnecke@gmail.com>
2020-08-17 17:18:11 -0600

    working

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM python:3.7-alpine3.8
 
 RUN pip install pudb
 
-COPY app/ /app
 WORKDIR /app
+COPY ./app /app
 
 EXPOSE 6900
 
--- app/example.py
+++ app/example.py
@@ -3,7 +3,8 @@ def sum_strings(a, b):
         return int(a) + int(b)
     except Exception:
         from pudb.remote import set_trace
-        set_trace(term_size=(160, 40), host='0.0.0.0', port=6900)
+
+        set_trace(term_size=(160, 40), host="0.0.0.0", port=6900)
         print(a)
         print(b)
         return float(a) + float(b)
